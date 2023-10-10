34a2fb7592297462981c987a0759e01c1c34d5e2
Lavish Kumar <lavishkumar1212@gmail.com>
2023-10-10 21:24:34 +0530

    Conditional Statements File Commited

---
+++ conditionals.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    #First question
+        $x = 0;
+    if ($x == 1)
+        if ($x >= 0)
+        print "true";
+    else
+        print "false";
+
+    echo "<br>";
+   
+    #Second question
+    $x = 40;
+    $y = 20;
+    if ($x == $y) {
+        echo "1";
+        
+    }elseif($x > $y) {
+        echo  "2";
+    } 
+    else{
+        echo  "3";
+    }
+?>
+</body>
+</html>
